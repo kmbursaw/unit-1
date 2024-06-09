4d9e6a7170ccc1ba5504d21be0f61b2f73d78129
Kevin Bursaw, EdD <132414922+kmbursaw@users.noreply.github.com>
2024-06-08 19:58:41 -0500

    Add files via upload

---
+++ Chapter03/js/main.js
@@ -0,0 +1,150 @@
+//declares table variable globally
+var table;
+
+//cities function
+function cities(){
+
+	//creates array for city and population
+	var cityPop = [
+	{ 
+		city: 'Madison',
+		population: 233209
+	},
+	{
+		city: 'Milwaukee',
+		population: 594833
+	},
+	{
+		city: 'Green Bay',
+		population: 104057
+	},
+	{
+		city: 'Superior',
+		population: 27244
+	}
+	];
+
+
+	//creates table
+	table = document.createElement("table");
+
+	//creates header row
+	var headerRow = document.createElement("tr");
+
+	//creates the header names
+	headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>");
+
+	//appends header names to header row
+	table.appendChild(headerRow);
+
+	//loops through the cityPop array and adds city and population for each row
+	for(var i = 0; i < cityPop.length; i++){
+		var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+		table.insertAdjacentHTML('beforeend',rowHtml);
+	}
+	
+	//appends table to mydiv in DOM
+	document.getElementById("mydiv").appendChild(table);
+
+	//returns array from function
+	return cityPop;
+
+};
+
+//delcares returned array as a variable from cities function
+var cityPop = cities();
+
+//function that adds the City Size column
+function addColumns(cityPop){
+    //adds a row for each item in cityPop
+    document.querySelectorAll("tr").forEach(function(row, i){
+		//adds header and then loops through cityPop to evaluate size of population and classify into either small, medium, or large city
+    	if (i == 0){
+    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+    	} else {
+			//citySize variable created wit parameters
+    		var citySize;
+
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+
+    		} else if (cityPop[i-1].population < 500000){
+    			citySize = 'Medium';
+
+    		} else {
+    			citySize = 'Large';
+    		}
+			//adds city sizes to the table
+			row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>');
+
+    	}
+    });
+}
+
+
+
+//function to add mouse over and click events to the table
+function addEvents(){
+
+	//adds the event listener actions
+	document.querySelector("table").addEventListener("mouseover", function(){
+		//sets color variable
+		var color = "rgb(";
+		//chooses random number multiplied by 255 to select a hue of random color
+		for (var i=0; i<3; i++){
+
+			var random = Math.round(Math.random() * 255);
+
+			color += random;
+
+			if (i<2){
+				color += ",";
+			
+			} else {
+				color += ")";
+			}
+		}
+	//changes the color of the text in the table
+	this.style.color = color;
+	
+	});
+
+	//adds function to bring up text if user clicks any part of the table
+	function clickme(){
+		//text that appears after the click
+		alert('Hey, you clicked me!');
+	};
+	//adds the click me feature from the clickme function
+	document.querySelector("table").addEventListener("click", clickme)
+}
+
+//gets new city size column to appear
+addColumns(cityPop);
+
+//calls the addEvents function to add the color changeing text and click me text
+addEvents();
+
+
+
+//debug ajax script
+//function to callback the data after it is loaded and converted
+function debugCallback(data){
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(data))
+};
+
+//funtion to fetch the MegaCities data and turn the response into a json file
+function debugAjax(){
+	// Fetch the GeoJSON data
+	fetch("data/MegaCities.geojson")
+		.then(function(response) {
+			return response.json();
+		})
+		//calls debugCallback function and inserts it into the mydiv while converting the data to a string
+		.then(function(data) {
+			debugCallback(data);
+			document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br>' + JSON.stringify(data));
+		})
+};
+
+//calls debug ajax function
+debugAjax()
